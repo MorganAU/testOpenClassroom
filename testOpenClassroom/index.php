4476bc5bc62ca78c7b4032a360fe571e26c569e9
MorganAU <augereau.morgan42@gmail.com>
2019-04-03 15:43:37 +0200

    Ajout d'un formulaire

--- testOpenClassroom/index.php
+++ testOpenClassroom/index.php
@@ -8,5 +8,11 @@
 	<? php 
 		echo 'Ceci est du code php'
 	?>
+
+	<form action="form.php" method="post">
+		<input type="text" name="texte" placeholder="Saisissez votre texte ici" />
+		<input type="submit" name="envoyer" value="Envoyer" />
+	</form>
+
 </body>
 </html>
